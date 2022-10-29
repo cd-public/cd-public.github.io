7cf092407c0165e331c5f4cf1558f25ef59dcb4b
cd <cd@desk.com>
2022-10-29 01:15:49 -0700

    weighting and exclusions

--- advising/code/code.js
+++ advising/code/code.js
@@ -4,21 +4,21 @@ const csslider = document.getElementById("csrange");
 const csdisplay = document.getElementById("csdisplay");
 var csSliderVal = csslider.value;
 
-const csnames = ["No Computer Science", "CS-151: Intro to Programming in Python or equivalent", "CS-152: Data Structures or equivalent"] ;
+const csnames = ["No Computer Science", "CS-151: Intro to Programming", "CS-152: Data Structures or equivalent"] ;
 
 csdisplay.innerHTML = csnames[csSliderVal];
 
 csslider.oninput = function() {
 	csdisplay.innerHTML = csnames[this.value];
 	table.innerHTML = "" ;
-	constructTable('#table')
+	constructTable('#table') ;
 }
 
 const calcslider = document.getElementById("calcrange");
 const calcdisplay = document.getElementById("calcdisplay");
 var calcSliderVal = calcslider.value;
 
-const calcnames = ["No Calculus", "Calculus I (Derivatives), such as MATH-150", "Calculus II (Integrals), such as MATH-152"] ;
+const calcnames = ["No Calculus", "MATH-150 / Calculus I / Derivatives", " MATH-152 / Calculus II / Integrals"] ;
 
 calcdisplay.innerHTML = calcnames[calcSliderVal];
 
@@ -29,7 +29,6 @@ calcslider.oninput = function() {
 }
 
 const box138 = document.getElementById("138");
-var math138Val = box138;
 
 box138.oninput = function() {
 	table.innerHTML = "" ;
@@ -37,7 +36,6 @@ box138.oninput = function() {
 }
 
 const box151 = document.getElementById("151");
-var data151Val = box151;
 
 box151.oninput = function() {
 	table.innerHTML = "" ;
@@ -45,7 +43,6 @@ box151.oninput = function() {
 }
 
 const box251 = document.getElementById("251");
-var math251Val = box251;
 
 box251.oninput = function() {
 	table.innerHTML = "" ;
@@ -53,385 +50,440 @@ box251.oninput = function() {
 }
 
 const box280 = document.getElementById("280");
-var math280Val = box280;
 
 box280.oninput = function() {
 	table.innerHTML = "" ;
 	constructTable('#table') ;
 }
 
+const boxexdata = document.getElementById("exdata");
+
+const boxexcs__ = document.getElementById("excs__");
+
+boxexcs__.oninput = function() {
+	table.innerHTML = "" ;
+	constructTable('#table') ;
+}
+
+boxexdata.oninput = function() {
+	table.innerHTML = "" ;
+	constructTable('#table') ;
+}
+
+const boxexmath = document.getElementById("exmath");
+
+boxexmath.oninput = function() {
+	table.innerHTML = "" ;
+	constructTable('#table') ;
+}
+
+const boxexrest = document.getElementById("exrest");
+
+boxexrest.oninput = function() {
+	table.innerHTML = "" ;
+	constructTable('#table') ;
+}
+
 const list = 
 [
-  {
-    "Prefix": "CS",
-    "Number": 151,
-    "Name": "Intro Programming With Python",
-    "Days": "MWF",
-    "Start": 910,
-    "End": 1010,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 1,
-    "DSrank": 2
-  },
-  {
-    "Prefix": "CS",
-    "Number": 151,
-    "Name": "Intro Programming With Python",
-    "Days": "MWF",
-    "Start": 1020,
-    "End": 1120,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 1,
-    "DSrank": 2
-  },
-  {
-    "Prefix": "CS",
-    "Number": 152,
-    "Name": "Data Structures",
-    "Days": "TR",
-    "Start": 800,
-    "End": 930,
-    "CSreq": 1,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 1,
-    "DSrank": 3
-  },
-  {
-    "Prefix": "CS",
-    "Number": 261,
-    "Name": "Software Development",
-    "Days": "MWF",
-    "Start": 910,
-    "End": 1010,
-    "CSreq": 2,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 2,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "CS",
-    "Number": 299,
-    "Name": "Top Human-Comp. Interaction",
-    "Days": "MW",
-    "Start": 1430,
-    "End": 1600,
-    "CSreq": 1,
-    "MATHreq": 0,
-    "Specialreq": "DATA151",
-    "CSrank": 4,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "CS",
-    "Number": 370,
-    "Name": "Fundamentals of Data Science",
-    "Days": "TR",
-    "Start": 1250,
-    "End": 1420,
-    "CSreq": 1,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 4,
-    "DSrank": 3
-  },
-  {
-    "Prefix": "CS",
-    "Number": 399,
-    "Name": "Top Computing 4 Social Good",
-    "Days": "TR",
-    "Start": 940,
-    "End": 1110,
-    "CSreq": 1,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 4,
-    "DSrank": 3
-  },
-  {
-    "Prefix": "DATA",
-    "Number": 151,
-    "Name": "Intro to Data Science with R",
-    "Days": "TR",
-    "Start": 1250,
-    "End": 1420,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 2
-  },
-  {
-    "Prefix": "DATA",
-    "Number": 152,
-    "Name": "Inferential Statistics With R",
-    "Days": "TR",
-    "Start": 940,
-    "End": 1110,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 2
-  },
-  {
-    "Prefix": "DATA",
-    "Number": 152,
-    "Name": "Inferential Statistics With R",
-    "Days": "TR",
-    "Start": 1250,
-    "End": 1420,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 2
-  },
-  {
-    "Prefix": "DATA",
-    "Number": 252,
-    "Name": "Models and Machine Learning",
-    "Days": "TR",
-    "Start": 1430,
-    "End": 1600,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "MATH280",
-    "CSrank": 5,
-    "DSrank": 1
-  },
-  {
-    "Prefix": "DATA",
-    "Number": 299,
-    "Name": "Top Data in the Cosmos",
-    "Days": "MW",
-    "Start": 1430,
-    "End": 1600,
-    "CSreq": 1,
-    "MATHreq": 0,
-    "Specialreq": "DATA151",
-    "CSrank": 5,
-    "DSrank": 3
-  },
-  {
-    "Prefix": "DATA",
-    "Number": 352,
-    "Name": "Ethics/Teamwork/Communication",
-    "Days": "MW",
-    "Start": 1250,
-    "End": 1420,
-    "CSreq": 1,
-    "MATHreq": 0,
-    "Specialreq": "DATA151",
-    "CSrank": 2,
-    "DSrank": 1
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 138,
-    "Name": "Statistics and Applications",
-    "Days": "MWF",
-    "Start": 910,
-    "End": 1010,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 2
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 138,
-    "Name": "Statistics and Applications",
-    "Days": "MWF",
-    "Start": 1020,
-    "End": 1120,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 2
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 152,
-    "Name": "Calculus II",
-    "Days": "MWF",
-    "Start": 1020,
-    "End": 1120,
-    "CSreq": 0,
-    "MATHreq": 1,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 152,
-    "Name": "Calculus II",
-    "Days": "MWF",
-    "Start": 1240,
-    "End": 1340,
-    "CSreq": 0,
-    "MATHreq": 1,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 153,
-    "Name": "Sequences and Series",
-    "Days": "MWF",
-    "Start": 1020,
-    "End": 1120,
-    "CSreq": 0,
-    "MATHreq": 2,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 153,
-    "Name": "Sequences and Series",
-    "Days": "MWF",
-    "Start": 1240,
-    "End": 1340,
-    "CSreq": 0,
-    "MATHreq": 2,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 249,
-    "Name": "Multivariable Calculus",
-    "Days": "MWF",
-    "Start": 1020,
-    "End": 1120,
-    "CSreq": 0,
-    "MATHreq": 2,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 251,
-    "Name": "Foundations of Advanced Math",
-    "Days": "MWF",
-    "Start": 910,
-    "End": 1010,
-    "CSreq": 0,
-    "MATHreq": 1,
-    "Specialreq": "",
-    "CSrank": 1,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 251,
-    "Name": "Foundations of Advanced Math",
-    "Days": "TR",
-    "Start": 1250,
-    "End": 1420,
-    "CSreq": 1,
-    "MATHreq": 1,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 253,
-    "Name": "Linear Algebra",
-    "Days": "TR",
-    "Start": 940,
-    "End": 1110,
-    "CSreq": 0,
-    "MATHreq": 2,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 3
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 376,
-    "Name": "Top Probability, Computing",
-    "Days": "TR",
-    "Start": 1250,
-    "End": 1420,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "MATH251",
-    "CSrank": 4,
-    "DSrank": 3
-  },
-  {
-    "Prefix": "MATH",
-    "Number": 376,
-    "Name": "Top Adv. Linear Algebra",
-    "Days": "MWF",
-    "Start": 910,
-    "End": 1010,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "MATH251",
-    "CSrank": 5,
-    "DSrank": 3
-  },
-  {
-    "Prefix": "ENVS",
-    "Number": 250,
-    "Name": "Geographic Information Systems",
-    "Days": "TR",
-    "Start": 1250,
-    "End": 1600,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "PHYS",
-    "Number": 346,
-    "Name": "Nonlinear Dynamics and Chaos",
-    "Days": "MWF",
-    "Start": 1020,
-    "End": 1120,
-    "CSreq": 0,
-    "MATHreq": 1,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 4
-  },
-  {
-    "Prefix": "PHIL",
-    "Number": 140,
-    "Name": "Symbolic Logic",
-    "Days": "MWF",
-    "Start": 1240,
-    "End": 1340,
-    "CSreq": 0,
-    "MATHreq": 0,
-    "Specialreq": "",
-    "CSrank": 5,
-    "DSrank": 5
-  }
+ {
+   "Prefix": "CS",
+   "Number": 151,
+   "Name": "Intro Programming With Python",
+   "Days": "MWF",
+   "Start": 910,
+   "End": 1010,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 4,
+   "DSrank": 1
+ },
+ {
+   "Prefix": "CS",
+   "Number": 151,
+   "Name": "Intro Programming With Python",
+   "Days": "MWF",
+   "Start": 1020,
+   "End": 1120,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 4,
+   "DSrank": 1
+ },
+ {
+   "Prefix": "CS",
+   "Number": 152,
+   "Name": "Data Structures",
+   "Days": "TR",
+   "Start": 800,
+   "End": 930,
+   "CSreq": 1,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 3,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "CS",
+   "Number": 261,
+   "Name": "Software Development",
+   "Days": "MWF",
+   "Start": 910,
+   "End": 1010,
+   "CSreq": 2,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 2,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "CS",
+   "Number": 299,
+   "Name": "Top Human-Comp. Interaction",
+   "Days": "MW",
+   "Start": 1430,
+   "End": 1600,
+   "CSreq": 1,
+   "MATHreq": 0,
+   "Specialreq": "DATA151",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "CS",
+   "Number": 370,
+   "Name": "Fundamentals of Data Science",
+   "Days": "TR",
+   "Start": 1250,
+   "End": 1420,
+   "CSreq": 1,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "CS",
+   "Number": 399,
+   "Name": "Top Computing 4 Social Good",
+   "Days": "TR",
+   "Start": 940,
+   "End": 1110,
+   "CSreq": 1,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "DATA",
+   "Number": 151,
+   "Name": "Intro to Data Science with R",
+   "Days": "TR",
+   "Start": 1250,
+   "End": 1420,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 1
+ },
+ {
+   "Prefix": "DATA",
+   "Number": 152,
+   "Name": "Inferential Statistics With R",
+   "Days": "TR",
+   "Start": 940,
+   "End": 1110,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 1
+ },
+ {
+   "Prefix": "DATA",
+   "Number": 152,
+   "Name": "Inferential Statistics With R",
+   "Days": "TR",
+   "Start": 1250,
+   "End": 1420,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 1
+ },
+ {
+   "Prefix": "DATA",
+   "Number": 252,
+   "Name": "Models and Machine Learning",
+   "Days": "TR",
+   "Start": 1430,
+   "End": 1600,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "MATH280",
+   "CSrank": 0,
+   "DSrank": 1
+ },
+ {
+   "Prefix": "DATA",
+   "Number": 299,
+   "Name": "Top Data in the Cosmos",
+   "Days": "MW",
+   "Start": 1430,
+   "End": 1600,
+   "CSreq": 1,
+   "MATHreq": 0,
+   "Specialreq": "DATA151",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "DATA",
+   "Number": 352,
+   "Name": "Ethics/Teamwork/Communication",
+   "Days": "MW",
+   "Start": 1250,
+   "End": 1420,
+   "CSreq": 1,
+   "MATHreq": 0,
+   "Specialreq": "DATA151",
+   "CSrank": 1,
+   "DSrank": 1
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 138,
+   "Name": "Statistics and Applications",
+   "Days": "MWF",
+   "Start": 910,
+   "End": 1010,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 1
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 138,
+   "Name": "Statistics and Applications",
+   "Days": "MWF",
+   "Start": 1020,
+   "End": 1120,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 1
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 152,
+   "Name": "Calculus II",
+   "Days": "MWF",
+   "Start": 1020,
+   "End": 1120,
+   "CSreq": 0,
+   "MATHreq": 1,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 152,
+   "Name": "Calculus II",
+   "Days": "MWF",
+   "Start": 1240,
+   "End": 1340,
+   "CSreq": 0,
+   "MATHreq": 1,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 153,
+   "Name": "Sequences and Series",
+   "Days": "MWF",
+   "Start": 1020,
+   "End": 1120,
+   "CSreq": 0,
+   "MATHreq": 2,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": -1
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 153,
+   "Name": "Sequences and Series",
+   "Days": "MWF",
+   "Start": 1240,
+   "End": 1340,
+   "CSreq": 0,
+   "MATHreq": 2,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": -1
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 249,
+   "Name": "Multivariable Calculus",
+   "Days": "MWF",
+   "Start": 1020,
+   "End": 1120,
+   "CSreq": 0,
+   "MATHreq": 2,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 251,
+   "Name": "Foundations of Advanced Math",
+   "Days": "MWF",
+   "Start": 910,
+   "End": 1010,
+   "CSreq": 0,
+   "MATHreq": 1,
+   "Specialreq": "",
+   "CSrank": 3,
+   "DSrank": -1
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 251,
+   "Name": "Foundations of Advanced Math",
+   "Days": "TR",
+   "Start": 1250,
+   "End": 1420,
+   "CSreq": 1,
+   "MATHreq": 1,
+   "Specialreq": "",
+   "CSrank": 3,
+   "DSrank": -1
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 253,
+   "Name": "Linear Algebra",
+   "Days": "TR",
+   "Start": 940,
+   "End": 1110,
+   "CSreq": 0,
+   "MATHreq": 2,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 376,
+   "Name": "Top Probability, Computing",
+   "Days": "TR",
+   "Start": 1250,
+   "End": 1420,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "MATH251",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "MATH",
+   "Number": 376,
+   "Name": "Top Adv. Linear Algebra",
+   "Days": "MWF",
+   "Start": 910,
+   "End": 1010,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "MATH251",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "ENVS",
+   "Number": 250,
+   "Name": "Geographic Information Systems",
+   "Days": "TR",
+   "Start": 1250,
+   "End": 1600,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "PHYS",
+   "Number": 346,
+   "Name": "Nonlinear Dynamics and Chaos",
+   "Days": "MWF",
+   "Start": 1020,
+   "End": 1120,
+   "CSreq": 0,
+   "MATHreq": 1,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": 0
+ },
+ {
+   "Prefix": "PHIL",
+   "Number": 140,
+   "Name": "Symbolic Logic",
+   "Days": "MWF",
+   "Start": 1240,
+   "End": 1340,
+   "CSreq": 0,
+   "MATHreq": 0,
+   "Specialreq": "",
+   "CSrank": 0,
+   "DSrank": -1
+ }
 ]
+	
+const majslider = document.getElementById("majrange");
+const majdisplay = document.getElementById("majdisplay");
+var majSliderVal = majslider.value;
 
-list.sort(function(a,b) { return (a.CSrank - b.CSrank) * 1000 + b.Number - a.Number  } )
+const majnames = ["Computer Science", "Computer and Data Science", "Data Science"] ;
+
+majdisplay.innerHTML = majnames[majSliderVal];
+
+majslider.oninput = function() {
+	majdisplay.innerHTML = majnames[this.value];
+	table.innerHTML = "" ;
+	if (this.value == 0) { list.sort(function(b,a) { return (a.CSrank - b.CSrank) ; } ) }
+	if (this.value == 2) { list.sort(function(b,a) { return (a.DSrank - b.DSrank) ; } ) }
+	if (this.value == 1) { list.sort(function(b,a) 
+	{ 
+		return ( (a.DSrank + a.CSrank) - (b.DSrank + b.CSrank) ) ; 
+	} ) }
+	constructTable('#table') ;
+}
 	
 function screen(list)
 {
+	// Excludes
+	if (boxexcs__.checked && list["Prefix"] == "CS") { return 0 ; }
+	if (boxexdata.checked && list["Prefix"] == "DATA") { return 0 ; }
+	if (boxexmath.checked && list["Prefix"] == "MATH") { return 0 ; }
+	if (boxexrest.checked && !(list["Prefix"] == "MATH" || list["Prefix"] == "DATA" || list["Prefix"] == "CS")) { return 0 ; }
+	
+	// Major checks
+	if (majslider.value == 1) { if ((list["CSrank"] + list["DSrank"]) < 1) { return 0 ; } }
+	if (majslider.value == 2) { if (list["DSrank"] < 0) { return 0 ; } }
+	
 	// Redundancies
 	if (box138.checked)
 	{
@@ -451,6 +503,11 @@ function screen(list)
 	if (list["Name"] == "Foundations of Advanced Math" && csslider.value > 0) { return 1 ; }
 	
 	// CS/Calc
+	
+	if (list["Name"] == "Intro Programming With Python" && csslider.value > 0) { return 0 ; }
+	if (list["Name"] == "Data Structures" && csslider.value > 1) { return 0 ; }
+	if (list["Name"] == "Calculus II" && calcslider.value > 1) { return 0 ; }	if (list["Prefix"] == "MATH" && (list["Number"] == 150 || list["Number"] == 151) && calcslider.value > 0) { return 0 ; }
+	
 	if (list["CSreq"] > csslider.value) { return 0 ; }
 	if (list["MATHreq"] > calcslider.value) { return 0 ; }
 	
@@ -470,6 +527,11 @@ function constructTable(selector) {
 			{
 				var val = list[i][cols[colIndex]];
 				
+				if (cols[colIndex] == "Start" || cols[colIndex] == "End")
+				{
+					if (val < 1000) { val = '0' + val.toString() ; }
+				}
+				
 				if (val == null) val = "";
 					row.append($('<td/>').html(val));
 			}
@@ -482,7 +544,7 @@ function constructTable(selector) {
 
 function Headers(list, selector) {
 	var columns = [];
-	var header = $('<tr/>');
+	var header = $('<tr />');
 	
 	for (var i = 0; i < list.length; i++) {
 		var row = list[i];
